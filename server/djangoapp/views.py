390337aad84c5a34aa19387ad189cc32aaa2424e
agustinsosa5 <agagustinsosa2563@gmail.com>
2023-08-07 20:20:24 -0400

    Cambios temporales

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,16 +15,23 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-
+def my_view(request):
+    return render(request, 'static_template.html')
 
 # Create an `about` view to render a static about page
 # def about(request):
-# ...
+def about_us(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/about.html', context)
 
 
 # Create a `contact` view to return a static contact page
 #def contact(request):
-
+def contact_us(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/contact.html', context)
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
 # ...
